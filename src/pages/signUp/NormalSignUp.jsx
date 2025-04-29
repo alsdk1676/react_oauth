240b7ae9023533ae9d97bda884813444b325a7e7
alsdk1676 <alsdk6761@gmail.com>
2025-04-29 13:58:24 +0900

    oauth 0429

--- src/pages/signUp/NormalSignUp.jsx
+++ src/pages/signUp/NormalSignUp.jsx
@@ -1,22 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 
-const SignIn = () => {
+const NormalSignUp = () => {
 
   const { register, handleSubmit, getValues, formState: {isSubmitting, isSubmitted, errors}} = useForm({mode:"onChange"});
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[!@#])[\da-zA-Z!@#]{8,}$/;
 
-  // 소셜 로그인
-  const navigateGoogleAuth = () => {
-    window.location.href = "http://localhost:10000/oauth2/authorization/google"
-  }
-  const navigateKaKaoAuth = () => {
-    window.location.href = "http://localhost:10000/oauth2/authorization/kakao"
-  }
-  const navigateNaverAuth = () => {
-    window.location.href = "http://localhost:10000/oauth2/authorization/naver"
-  }
 
   return (
     <form onSubmit={handleSubmit(async (data) => {
@@ -60,14 +50,42 @@ const SignIn = () => {
         )}
       </label>
 
-      <button disabled={isSubmitting}>로그인</button>
-      <div>
-        <button onClick={navigateGoogleAuth}>구글 로그인</button>
-        <button onClick={navigateKaKaoAuth}>카카오 로그인</button>
-        <button onClick={navigateNaverAuth}>네이버 로그인</button>
-      </div>
+      <label>
+        <p>비밀번호 확인</p>
+        <input type="password" id='passwordConfirm' placeholder='비밀번호를 입력하세요.' 
+          {...register("passwordConfirm", {
+            required : true,
+            validate : {
+              matchPassword : (passwordConfirm) => {
+                const { memberPassword } = getValues();
+                console.log(memberPassword, passwordConfirm, memberPassword === passwordConfirm );
+                return memberPassword === passwordConfirm;
+              }
+            }
+          })}
+        />
+        {errors && errors?.passwordConfirm && (
+          <p>비밀번호가 일치하지 않습니다.</p>
+        )}
+      </label>
+
+      <label>
+        <p>이름</p>
+        <input type="text" placeholder='이름을 입력하세요.' 
+          {...register("memberName")}
+        />
+      </label>
+
+      <label>
+        <p>닉네임</p>
+        <input type="text" placeholder='닉네임을 입력하세요.' 
+          {...register("memberNickName")}
+        />
+      </label>
+
+      <button disabled={isSubmitting}>회원가입</button>
     </form>
   );
 };
 
-export default SignIn;
+export default NormalSignUp;
---
+++ src/pages/signUp/SocialSignUp.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+const SocialSignUp = ({email, provider}) => {
+
+  const { register, handleSubmit, getValues, formState: {isSubmitting, isSubmitted, errors}} = useForm({mode:"onChange"});
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[!@#])[\da-zA-Z!@#]{8,}$/;
+
+  return (
+    <form onSubmit={handleSubmit(async (data) => {
+
+      const {memberName, memberNickName} = data;
+
+      const oauthMemberVO = {
+        memberEmail : email,
+        memberName : memberName,
+        memberNickName : memberNickName,
+        memberProvider : provider
+      }
+
+      console.log("oauthMemberVO", oauthMemberVO)
+
+      // register 요청 보냄 (최초 로그인한 사용자 => register로 요청)
+      await fetch("http://localhost:10000/member/register", {
+        method : "POST",
+        headers : {
+          "Content-Type" : "application/json"
+        },
+        body : JSON.stringify(oauthMemberVO)
+      })
+      .then((res) => {
+        if(!res.ok) {
+          return res.json().then((res) => {
+            alert(`${res.message}(${res.provider})`)
+          })
+        }
+        return res.json()
+      })
+      // 성공한 res
+      .then((res) => {
+        console.log(res)
+      })
+      .catch(console.error)
+
+    })}>
+      <label>
+        <p>이름</p>
+        <input type="text" placeholder='이름을 입력하세요.' 
+          {...register("memberName")}
+        />
+      </label>
+
+      <label>
+        <p>닉네임</p>
+        <input type="text" placeholder='닉네임을 입력하세요.' 
+          {...register("memberNickName")}
+        />
+      </label>
+
+      <button disabled={isSubmitting}>회원가입</button>
+    </form>
+  );
+};
+
+export default SocialSignUp;
